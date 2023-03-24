f3090de319d0cce6018592e0fc1079230bab5f60
YoryinaOrtegaU <yoryina.ortega@mercadolibre.com.co>
2023-03-24 11:27:22 -0500

    Arrays

---
+++ arrays/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// for range
+func RecorrerSlice(array [3]string) {
+	for i, value := range array {
+		fmt.Println(i, value)
+	}
+}
+
+func main() {
+	// for range
+	var frutas [3]string
+	frutas[0] = "lulo"
+	frutas[1] = "pera"
+	frutas[2] = "mora"
+
+	RecorrerSlice(frutas)
+
+	var a [3]string
+	a[0] = "hola"
+	a[1] = "hora"
+	a[2] = "ola"
+
+	fmt.Println(a[0])
+	fmt.Println(a)
+}
