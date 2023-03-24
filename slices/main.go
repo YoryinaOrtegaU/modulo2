fc25e154b01efdcc7316347bc7f701b1d65e3daf
YoryinaOrtegaU <yoryina.ortega@mercadolibre.com.co>
2023-03-24 13:36:09 -0500

    slices

---
+++ slices/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//slice
+	var sl []int
+	sl = append(sl, 4, 1, 2)
+
+	fmt.Println("sl", sl[1:2], cap(sl), len(sl))
+
+	var ola = []int{2, 3, 4}
+	ola = append(ola, 3, 6, 7, 8, 9)
+	fmt.Println("ola", ola, ola[:3], cap(ola), len(ola))
+	fmt.Println("posicion 1", ola[1])
+
+	nombres := make([]string, 5)
+	nombres = append(nombres, "esperancito")
+	fmt.Println("nombres", nombres, cap(nombres), len(nombres))
+}
